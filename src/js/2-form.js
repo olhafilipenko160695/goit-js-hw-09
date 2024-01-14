0bb86444374026f882bb9d32e2a192ab01cfc90a
olhafilipenko160695 <olyaao95@gmail.com>
2024-01-14 20:31:52 +0100

    final changes

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,2 +1,36 @@
+const STORAGE_KEY = 'feedback-form-state';
 const form = document.querySelector('.feedback-form');
-const email = document.querySelector;
+const email = form.elements.email;
+const message = form.elements.message;
+
+form.addEventListener('input', onInput);
+form.addEventListener('submit', onSubmit);
+
+function onInput() {
+  const userData = {
+    email: email.value,
+    message: message.value,
+  };
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(userData));
+}
+
+const storedData = localStorage.getItem(STORAGE_KEY);
+if (storedData) {
+  const parsedData = JSON.parse(storedData);
+  email.value = parsedData.email;
+  message.value = parsedData.message;
+}
+
+function onSubmit(evt) {
+  evt.preventDefault();
+  if (email.value.trim() && message.value.trim()) {
+    const formData = {
+      email: email.value,
+      message: message.value,
+    };
+    console.log(formData);
+
+    localStorage.removeItem(STORAGE_KEY);
+    form.reset();
+  }
+}
